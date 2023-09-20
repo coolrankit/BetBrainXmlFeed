b5b86c5c1c6346794163f95d4681f6a42d10f3b3
coolrankit <coolrankit@users.noreply.github.com>
2023-09-20 22:33:34 +0530

    Update rdb.php

--- rdb.php
+++ rdb.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_NAME', '003');
-define('DB_USER', 'root');
+define('DB_NAME', '');
+define('DB_USER', '');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_PREFIX', 'fd_');
